830fb88094552a79a6c0ddce9f156903eb07dc56
Guillermo Arispe <guillermo.arispe@gmail.com>
2022-08-17 11:12:58 -0300

    Updated Settings dialog

--- src/SettingsDialog/index.js
+++ src/SettingsDialog/index.js
@@ -5,51 +5,78 @@ import Dialog from "@mui/material/Dialog"
 import DialogTitle from "@mui/material/DialogTitle"
 import DialogContent from "@mui/material/DialogContent"
 import DialogActions from "@mui/material/DialogActions"
+import FormControl from "@mui/material/FormControl"
+import FormLabel from "@mui/material/FormLabel"
+import RadioGroup from "@mui/material/RadioGroup"
+import Radio from "@mui/material/Radio"
+import FormControlLabel from "@mui/material/FormControlLabel"
 import Button from "@mui/material/Button"
-import Survey from "material-survey/components/Survey"
 import { useSettings } from "../SettingsProvider"
+import { Stack } from "@mui/material"
 
 export const SettingsDialog = ({ open, onClose }) => {
   const settings = useSettings()
   return (
-    <Dialog open={open || false} onClose={onClose}>
-      <DialogTitle>Configuración</DialogTitle>
-      <DialogContent style={{ minWidth: 400 }}>
-        <Survey
-          variant="flat"
-          noActions
-          defaultAnswers={settings}
-          onQuestionChange={(q, a, answers) => settings.changeSetting(q, a)}
-          form={{
-            questions: [
-              {
-                type: "boolean",
-                title: "Mostrar puntos de mira",
-                name: "showCrosshairs",
-              },
-              {
-                type: "boolean",
-                title: "Mostrar cuadro resaltado",
-                name: "showHighlightBox",
-              },
-              // {
-              //   type: "boolean",
-              //   title: "WASD Mode",
-              //   name: "wasdMode",
-              // },
-              // {
-              //   type: "dropdown",
-              //   title: "Video Playback Speed",
-              //   name: "videoPlaybackSpeed",
-              //   defaultValue: "1x",
-              //   choices: ["0.25x", "0.5x", "1x", "2x"],
-              // },
-            ],
-          }}
-        />
+    <Dialog
+      fullWidth
+      open={open}
+      onClose={() => {}}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{"Configuración"}</DialogTitle>
+      <DialogContent>
+        <Stack>
+          <FormControl>
+            <FormLabel>Mostrar punto de mira</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+            >
+              <FormControlLabel
+                checked={settings.showCrosshairs}
+                control={<Radio />}
+                label="Si"
+                onChange={() => settings.changeSetting("showCrosshairs", true)}
+              />
+              <FormControlLabel
+                checked={!settings.showCrosshairs}
+                control={<Radio />}
+                label="No"
+                onChange={() => settings.changeSetting("showCrosshairs", false)}
+              />
+            </RadioGroup>
+          </FormControl>
+        </Stack>
+        <Stack>
+          <FormControl>
+            <FormLabel>Mostrar cuadro resaltado</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+            >
+              <FormControlLabel
+                checked={settings.showHighlightBox}
+                control={<Radio />}
+                label="Si"
+                onChange={() =>
+                  settings.changeSetting("showHighlightBox", true)
+                }
+              />
+              <FormControlLabel
+                checked={!settings.showHighlightBox}
+                control={<Radio />}
+                label="No"
+                onChange={() =>
+                  settings.changeSetting("showHighlightBox", false)
+                }
+              />
+            </RadioGroup>
+          </FormControl>
+        </Stack>
       </DialogContent>
       <DialogActions>
-        <Button onClick={onClose}>Cerrar</Button>
+        <Button onClick={onClose}>cerrar</Button>
       </DialogActions>
     </Dialog>
   )
